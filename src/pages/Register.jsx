c6bdc17e0d742e83180819e291b5fc68681f5322
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2026-08-04 19:01:45 +0530

    Fix Navigate import

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 import { registerUser } from "../services/authService";
