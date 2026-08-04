3aeb3147cd7702fcb577a24ebf1595f4224a1237
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2026-08-04 14:20:26 +0530

    added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://url-shortener-service-rots.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
